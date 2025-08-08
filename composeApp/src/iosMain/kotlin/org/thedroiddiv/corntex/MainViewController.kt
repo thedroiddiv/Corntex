704b9c4dd893b304127524d6f1d47ff7f80e1c63
Divyansh Kushwaha <thedroiddiv@gmail.com>
2025-08-08 18:01:43 +0530

    batman

---
+++ composeApp/src/iosMain/kotlin/org/thedroiddiv/corntex/MainViewController.kt
@@ -0,0 +1,5 @@
+package org.thedroiddiv.corntex
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
---
+++ composeApp/src/jvmMain/kotlin/org/thedroiddiv/corntex/main.kt
@@ -0,0 +1,13 @@
+package org.thedroiddiv.corntex
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Corntex",
+    ) {
+        App()
+    }
+}
